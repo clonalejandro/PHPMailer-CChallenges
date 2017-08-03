9e447dc51d94eb1e2da42bf37f3ebfc361d545b1
clonalejandro <contact@clonalejandro.me>
2017-08-03 18:36:26 +0200

    Create Mailer.php

---
+++ Mailer.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by alejandrorioscalera
+ * 03/08/2017
+ *
+ * -- SOCIAL NETWORKS --
+ *
+ * GitHub: https://github.com/clonalejandro or @clonalejandro
+ * Website: https://clonalejandro.me/
+ * Twitter: https://twitter.com/clonalejandro11/ or @clonalejandro11
+ * Keybase: https://keybase.io/clonalejandro/
+ *
+ * -- LICENSE --
+ *
+ * All rights reserved for clonalejandro ©Mailer 2017 / 2018
+ */
+
+/**
+ * @param $sender
+ * @param $receiver
+ * @param $subject
+ * @param $content
+ * @param $html
+ */
+function sendMail($sender, $receiver, $subject, $content, $html){
+
+    $headers = "From: $sender\r\n";
+    $headers .= "Reply-To: $sender\r\n";
+
+    if ($html)
+        $headers .= "Content-type: text-html; charset=utf-8"."\r\n";
+    
+    mail($receiver, $subject, $content, $headers);
+}
