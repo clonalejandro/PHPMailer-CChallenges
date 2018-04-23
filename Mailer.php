6a0b136f6d65b3ef6eab80f66ddbb87199d4564e
clonalejandro <contact@clonalejandro.me>
2018-04-23 22:23:22 +0200

    Update Mailer.php

--- Mailer.php
+++ Mailer.php
@@ -1,7 +1,10 @@
 <?php
+
+namespace io\clonalejandro\Mailer;
+
 /**
  * Created by alejandrorioscalera
- * 03/08/2017
+ * 23/04/2018
  *
  * -- SOCIAL NETWORKS --
  *
@@ -12,22 +15,103 @@
  *
  * -- LICENSE --
  *
- * All rights reserved for clonalejandro ©Mailer 2017 / 2018
+ * All rights reserved for clonalejandro ©untitled 2017 / 2018
  */
 
+class Mailer {
 
-/**
- * @param $sender
- * @param $receiver
- * @param $subject
- * @param $content
- * @param $html
- */
-function sendMail($sender, $receiver, $subject, $content, $html){
-    $headers = "From: $sender\r\n";
-    $headers .= "Reply-To: $sender\r\n";
-    if ($html)
-        $headers .= "Content-Type: text/html; charset=\"iso-8859-1\" Content-Transfer-Encoding: quoted-printable; charset=utf-8"."\r\n";
 
-    mail($receiver, $subject, $content, $headers);
+    /** SMALL CONSTRUCTORS **/
+
+    private $sender, $receiver, $subject, $content, $html;
+
+    /**
+     * Mailer constructor.
+     * @param  $sender string
+     * @param $receiver string
+     * @param $subject string
+     * @param $content string
+     * @param $html boolean
+     */
+    public function __construct($sender, $receiver, $subject, $content, $html)
+    {
+        //Define vars
+        $this->sender = $sender;
+        $this->receiver = $receiver;
+        $this->subject = $subject;
+        $this->content = $content;
+        $this->html = $html;
+
+        //Call to function
+        $this->sendMail();
+    }
+
+
+    /** REST **/
+
+    /**
+     * This function returns a mail sender
+     * @return string
+     */
+    public function getSender()
+    {
+        return $this->sender;
+    }
+
+
+    /**
+     * This function returns a mail receiver
+     * @return string
+     */
+    public function getReceiver()
+    {
+        return $this->receiver;
+    }
+
+
+    /**
+     * This function returns a mail subject
+     * @return string
+     */
+    public function getSubject()
+    {
+        return $this->subject;
+    }
+
+
+    /**
+     * This function returns a mail content
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+
+    /**
+     * This function returns if content of mail is in html format
+     * @return bool || @return boolean
+     */
+    public function isHtml()
+    {
+        return $this->html;
+    }
+
+
+    /** OTHERS **/
+
+    /**
+     * This function send a mail with constructor parameters
+     */
+    private function sendMail()
+    {
+        $headers = $this->html ?
+            "From: $this->sender\r\n"."Reply-To: $this->sender\r\n" :
+            "From: $this->sender\r\n"."Reply-To: $this->sender\r\n"."Content-Type: text/html; charset=\"iso-8859-1\" Content-Transfer-Encoding: quoted-printable; charset=utf-8"."\r\n";
+
+        mail($this->receiver, $this->subject, $this->content, $headers);
+    }
+
+
 }
